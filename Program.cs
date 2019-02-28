974fc4f812337b5e6387a991ce36aa74e82da365
nguyentrongtiep <48078108+nguyentrongtiep@users.noreply.github.com>
2019-02-28 10:45:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // bai 1
+            Class1 tong = new Class1();
+            tong.nhapn();
+            tong.tinhTong();
+            tong.inTong();
+            tong.tinhTong2();
+            tong.tinhTong3();
+            tong.tongUoc();
+
+            //bai2
+            tientau tau = new tientau();
+            tau.nhapthongtin();
+            tau.tinhtien();
+            tau.Intau();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tientau.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class tientau
+    {
+        int soluong;
+        String matau;
+        public void nhapthongtin()
+        {
+            Console.WriteLine("nhap ma tau");
+            this.matau = Console.ReadLine();
+            Console.WriteLine("nhap so luong");
+            this.soluong = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public float tinhtien()
+        {
+            float S = 0;
+            if (this.soluong < 100)
+            {
+                S = this.soluong * 100000;
+            }
+            else
+            {
+                S = this.soluong * 80000;
+            }
+            return S;
+        }
+        public void Intau()
+        {
+            Console.WriteLine(this.matau);
+            Console.WriteLine(this.matau);
+            Console.WriteLine(tinhtien());
+            Console.WriteLine("tong tien la");
+        }
+    }
+}
